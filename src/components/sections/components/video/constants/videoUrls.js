c226d659e8bdd98a95611e51853f3648bb31b11c
p.pribysh <p.pribysh@itransition.com>
2020-08-08 11:57:49 +0300

    Add real video urls

--- src/components/sections/components/video/constants/videoUrls.js
+++ src/components/sections/components/video/constants/videoUrls.js
@@ -1,6 +1,6 @@
 export const VIDEO_URLS = {
-    pavel: 'https://www.youtube.com/watch?v=gvOWADwCDNg',
-    igor: 'https://www.youtube.com/watch?v=jhj63pWel2Q',
-    alexander: 'https://www.youtube.com/watch?v=ck9yB10Cy78',
-    yaroslav: 'https://www.youtube.com/watch?v=Ahhxk3uZE2g&t=8s',
+    pavel: 'https://youtu.be/Y2Hjm8KpHP4',
+    igor: 'https://youtu.be/zMgpOL2_uBg',
+    alexander: 'https://youtu.be/BJKJ0hppNEw',
+    yaroslav: 'https://youtu.be/eT7v8NiMxvM',
 };
